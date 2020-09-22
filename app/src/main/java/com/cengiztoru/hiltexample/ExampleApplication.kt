40724432ba02f1e5d618581e1bcfe295cb35fc65
Cengiz TORU <cengiztoru@gmail.com>
2020-09-22 22:38:05 +0300

    02 Hilt Android Application Prepared

---
+++ app/src/main/java/com/cengiztoru/hiltexample/ExampleApplication.kt
@@ -0,0 +1,12 @@
+package com.cengiztoru.hiltexample
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+
+/**
+ * Created by Cengiz TORU on 22/09/2020.
+ * cengiz.toru@tsoft.com.tr
+ */
+@HiltAndroidApp
+class ExampleApplication : Application()
