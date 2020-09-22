d17d320aa1b3ad0c81ac3252c4631b9effa618f3
Cengiz TORU <cengiztoru@gmail.com>
2020-09-22 22:53:52 +0300

    Removing manual created instance. After that injected viewmodel with its dependencies

--- app/src/main/java/com/cengiztoru/hiltexample/ui/main/MainActivity.kt
+++ app/src/main/java/com/cengiztoru/hiltexample/ui/main/MainActivity.kt
@@ -1,23 +1,30 @@
 package com.cengiztoru.hiltexample.ui.main
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import com.cengiztoru.hiltexample.R
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_main.*
 
 @AndroidEntryPoint                              //todo dont forget this 😊
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var viewModel: MainViewModel
+//    private lateinit var viewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         //By manual constructor injection all dependencies are gathered together
-        val repository = MainRepository()
-        val factory = MainViewModelFactory(repository)
-        viewModel = ViewModelProvider(this, factory).get(MainViewModel::class.java)
+//        val repository = MainRepository()
+//        val factory = MainViewModelFactory(repository)
+//        viewModel = ViewModelProvider(this, factory).get(MainViewModel::class.java)
+//
+
+        //OF COURSE NOW WE CAN DELETE MAIN VIEW MODEL FACTORY
+
+        val viewModel: MainViewModel by viewModels()
         viewModel.getUsers().observe(this, {
             tvMain.text = it.toString()
         })
