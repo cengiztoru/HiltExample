3706b245650de006ebca412270cb97ec3ee40818
Cengiz TORU <cengiztoru@gmail.com>
2020-09-22 22:43:09 +0300

    MainRepository Prepared For Injection

--- app/src/main/java/com/cengiztoru/hiltexample/ui/main/MainRepository.kt
+++ app/src/main/java/com/cengiztoru/hiltexample/ui/main/MainRepository.kt
@@ -2,14 +2,28 @@ package com.cengiztoru.hiltexample.ui.main
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import com.cengiztoru.hiltexample.data.model.User
+import com.cengiztoru.hiltexample.util.SharedHelper
+import javax.inject.Inject
 
 
 /**
  * Created by Cengiz TORU on 22/09/2020.
  * cengiz.toru@tsoft.com.tr
  */
-class MainRepository {
+class MainRepository @Inject constructor(private val sharedHelper: SharedHelper) {
+
+    /**
+     * For example MainRepository need an instance of SharedPreferences or
+     * a retrofit instance.
+     * */
+
     fun getUsers(): LiveData<List<User>> {
+
+        /** you can use incoming instance like below*/
+//        sharedHelper.getString(key,"defaultValue")
+
+        /** For simple example I'll use mock data 👍*/
         val users = ArrayList<User>()
         users.add(User(1, "Cengiz", "TORU", 22))
         users.add(User(2, "Cengiz", "TORU", 22))
---
+++ app/src/main/java/com/cengiztoru/hiltexample/util/SharedHelper.kt
@@ -0,0 +1,11 @@
+package com.cengiztoru.hiltexample.util
+
+import android.content.Context
+import javax.inject.Inject
+
+
+/**
+ * Created by Cengiz TORU on 22/09/2020.
+ * cengiz.toru@tsoft.com.tr
+ */
+class SharedHelper @Inject constructor(context: Context)
