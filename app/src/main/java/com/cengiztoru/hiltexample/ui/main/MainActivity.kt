266cb4302a4343ca5f75a572f3c83fd4a1d36afc
Cengiz TORU <cengiztoru@gmail.com>
2020-09-22 22:52:16 +0300

    MainActivity.kt prepared for injection wiht @AndroidEndPoint

--- app/src/main/java/com/cengiztoru/hiltexample/ui/main/MainActivity.kt
+++ app/src/main/java/com/cengiztoru/hiltexample/ui/main/MainActivity.kt
@@ -2,9 +2,10 @@ package com.cengiztoru.hiltexample.ui.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModelProvider
+import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_main.*
 
+@AndroidEntryPoint                              //todo dont forget this 😊
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: MainViewModel
