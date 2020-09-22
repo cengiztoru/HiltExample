13d45ec140b71133c15115bbb742f919d076ea8a
Cengiz TORU <cengiztoru@gmail.com>
2020-09-22 22:45:17 +0300

    ViewModel Prepared For Injection

--- app/src/main/java/com/cengiztoru/hiltexample/ui/main/MainViewModel.kt
+++ app/src/main/java/com/cengiztoru/hiltexample/ui/main/MainViewModel.kt
@@ -1,14 +1,17 @@
 package com.cengiztoru.hiltexample.ui.main
 
+import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import com.cengiztoru.hiltexample.data.model.User
 
 
 /**
  * Created by Cengiz TORU on 22/09/2020.
  * cengiz.toru@tsoft.com.tr
  */
-class MainViewModel(private val repository: MainRepository) : ViewModel() {
+class MainViewModel @ViewModelInject constructor(private val repository: MainRepository) :
+    ViewModel() {
     fun getUsers(): LiveData<List<User>> {
         return repository.getUsers()
     }
